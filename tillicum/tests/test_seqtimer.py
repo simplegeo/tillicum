e8cdebdd879141ff1d5dddd2224ee0dbc842fb6b
Ian Eure <ian.eure@gmail.com>
2011-09-27 16:34:28 -0700

    Added test_summary, clean up test_warns.

--- tillicum/tests/test_seqtimer.py
+++ tillicum/tests/test_seqtimer.py
@@ -24,11 +24,16 @@ class SeqTimerTest(unittest.TestCase):
 
         self.assertEqual(len(out.getvalue().split("\n")), 11)
 
-    def test_warns(self):
-        # def generic_iter(inp):
-        #     for elt in inp:
-        #         yield elt
+    def test_summary(self):
+        items = range(100)
+        out = StringIO()
+        seq = st.seqtimer(items, output=out, summary=True)
+        for (idx, item) in enumerate(seq):
+            self.assertEqual(items[idx], item)
 
+        self.assertTrue("Finished processing" in out.getvalue())
+
+    def test_warns(self):
         out = StringIO()
         seq = st.seqtimer(iter(range(10)), output=out, summary=False,
                           length=1)
